cdd95d3799ab4159b6d28d63e87439b0e0dbe92f
johnguiscool <johnguiscool@gmail.com>
2019-03-30 22:54:53 -0400

    basic front end for stories

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Story;
+
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,21 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index() {
+    	$stories = Story::all();
+
+    	echo "<ul>";
+    	foreach($stories as $story)
+    	{
+    		echo "<li>$story->title</li>";
+    	}
+    	echo "</ul>";
+    }
+
+    public function displayStory($id) {
+    	$story = Story::where('id',$id)->get()->first();
+
+    	echo $story->content;
+    }
 }
---
+++ database/migrations/2019_03_31_014657_create_stories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
+            $table->text('summary')->nullable();
+            $table->double('price', 8, 2)->nullable()->default(0.00);
+            $table->boolean('is_published')->default(false);
+            $table->boolean('is_premium')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+}
